46eeb7f4a725340ee951a7f038a35186e609e789
raff-fradio <fradior.xd@gmail.com>
2021-10-14 15:33:55 +0700

    Finish login page

--- app/src/main/java/com/example/mobproglabquiz1/RegisterPage.java
+++ app/src/main/java/com/example/mobproglabquiz1/RegisterPage.java
@@ -4,22 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class RegisterPage extends AppCompatActivity {
+
+    Button registerButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_page);
 
-        goToLoginPage();
+        registerButton = findViewById(R.id.activity_register_page_register_button);
+        registerButton.setOnClickListener((view) -> {
+            Toast.makeText(this, "Successfully registered!", Toast.LENGTH_LONG);
+            finish();
+        });
     }
-
-    private void registerPageForm(String firstName, String lastName, String email, String password) {
-        Intent intent = new Intent();
-
-        
-    }
-
 
 }
