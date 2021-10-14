2bc7e714f5c6af313e2ab3ba4986f7b87b8409ae
raff-fradio <fradior.xd@gmail.com>
2021-10-14 14:28:58 +0700

    Delete unnecessary files

--- app/src/main/java/com/example/mobproglabquiz1/LoginPage.java
+++ app/src/main/java/com/example/mobproglabquiz1/LoginPage.java
@@ -7,15 +7,9 @@ import android.os.Bundle;
 
 public class LoginPage extends AppCompatActivity {
 
-    private AppBarConfiguration appBarConfiguration;
-//    private ActivityListPageBinding binding;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
-
-//        binding = ActivityListPageBinding.inflate(getLayoutInflater());
-//        setContentView(binding.getRoot());
     }
 }
