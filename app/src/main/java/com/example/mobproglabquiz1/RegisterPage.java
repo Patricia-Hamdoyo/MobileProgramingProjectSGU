c664f257403244790319bbfdc6df1d50bcf3ad75
Patricia-Hamdoyo <60873600+Patricia-Hamdoyo@users.noreply.github.com>
2021-10-14 15:06:54 +0700

    Update RegisterPage.java

--- app/src/main/java/com/example/mobproglabquiz1/RegisterPage.java
+++ app/src/main/java/com/example/mobproglabquiz1/RegisterPage.java
@@ -2,6 +2,7 @@ package com.example.mobproglabquiz1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class RegisterPage extends AppCompatActivity {
@@ -11,10 +12,14 @@ public class RegisterPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_page);
 
-        registerPageForm();
+        goToLoginPage();
     }
 
-    private void registerPageForm(String firstName, String lastName, String email, Password password) {
+    private void registerPageForm(String firstName, String lastName, String email, String password) {
+        Intent intent = new Intent();
 
+        
     }
+
+
 }
