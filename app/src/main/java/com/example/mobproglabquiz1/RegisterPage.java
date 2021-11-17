378659d2e2c3054f4fdd9ccf376abdd235635d7c
Patricia-Hamdoyo <60873600+Patricia-Hamdoyo@users.noreply.github.com>
2021-11-17 12:55:21 +0700

    update 17/11

--- app/src/main/java/com/example/mobproglabquiz1/RegisterPage.java
+++ app/src/main/java/com/example/mobproglabquiz1/RegisterPage.java
@@ -4,15 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class RegisterPage extends AppCompatActivity {
+
+    Button registerButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_page);
 
-//        goToLoginPage();
+        registerButton = findViewById(R.id.activity_register_page_register_button);
     }
 
     private void registerPageForm(String firstName, String lastName, String email, String password) {
